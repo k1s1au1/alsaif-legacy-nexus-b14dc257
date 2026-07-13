a75131f79d34e0c0a30c0ff25c97fc661fe8a648
k1s1au1 <k1s1au1@gmail.com>
2026-07-13 15:02:50 +0300

    Restore project to pre-SOS baseline

--- android/app/src/main/java/com/alsaif/familyhub/MainActivity.java
+++ android/app/src/main/java/com/alsaif/familyhub/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(WidgetPlugin.class);
         registerPlugin(SharingPlugin.class);
         registerPlugin(ContactsPlugin.class);
+        registerPlugin(SOSPlugin.class);
         registerPlugin(BiometricPlugin.class);
         super.onCreate(savedInstanceState);
     }
